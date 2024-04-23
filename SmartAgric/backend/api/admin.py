030c7737e11333918adf4a95a892d3d89a1543a7
abdo-bahi <101672488+abdo-bahi@users.noreply.github.com>
2024-04-23 20:01:06 +0100

    Delete SmartAgric/backend directory

--- SmartAgric/backend/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User, SensorData, Action, MCU
-
-# Register your models here.
-
-admin.site.register(SensorData)
-admin.site.register(Action)
-admin.site.register(MCU)
-admin.site.register(User)
